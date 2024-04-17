e0836a8fe227c59fc93a1a1bea79df8280c4252e
axido545 <axido545@gmail.com>
2024-04-17 19:59:06 +0200

    add nav css

--- components/header.tsx
+++ components/header.tsx
@@ -6,5 +6,9 @@ export default function header() {
      rounded-none border-white border-opacity-40 bg-white
       bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem]
       sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full'></div>
+    <nav className="fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/
+    2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+      <ul></ul>
+    </nav>
   </header>
 }
