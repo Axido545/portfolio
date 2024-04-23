0170b98c2303046e26634a224fd5182dcb09b97f
axido545 <axido545@gmail.com>
2024-04-23 19:18:16 +0200

    add imahe next component

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
-import {img1} from '../app/31qfm392.bmp'
 
 export default function intro() {
   return (
     <section>
       <div className='flex items-center justify-center '>
         <div>
-        <Image src={img1} alt='myimg'/>
+        <Image src='/31qfm392.bmp' width="192" height="192" quality="80" priority={true} alt='myimg'/>
         </div>
       </div>
     </section>
