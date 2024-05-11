1268a547cff3c26d06cf5df1bed09e779ad7bd17
axido545 <axido545@gmail.com>
2024-05-11 22:16:28 +0200

    css

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,16 +5,23 @@ export default function intro() {
   return (
     <section>
       <div className='flex items-center justify-center '>
-        <div>
-        <Image
-              src='/31qfm392.bmp' 
+        <div className='relative'>
+          <div>
+            <Image
+              src='/31qfm392.bmp'
               alt="flower"
               width="192"
               height="192"
               quality="95"
               priority={true}
-              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+              className="h-16 w-16 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
             />
+          </div>
+
+          <span className='absolute bottom-0
+            right-0 text-4xl'>
+            👋
+          </span>
           {/* <Image src='/31qfm392.bmp' width="192" height="192" quality="80" priority={true} alt='myimg' /> */}
         </div>
       </div>
@@ -22,3 +29,4 @@ export default function intro() {
   )
 }
 // https://unsplash.com/fr/photos/photographie-en-gros-plan-de-fleur-a-petales-rouges-et-blancs-w1JE5duY62M
+
